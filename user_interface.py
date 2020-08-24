bf4dde33d80ec48b3b923cd4bc852a86f56cb61f
Jordan Alwon <jordan.alwon@student.jade-hs.de>
2020-08-24 10:13:31 +0200

    Add Month-Overview widget

--- user_interface.py
+++ user_interface.py
@@ -47,7 +47,7 @@ class EditEntry(QtWidgets.QWidget):
         # Work start time
         start_time_label = set_default_label_style(QtWidgets.QLabel("Beginn:"))
         start_time_field = set_default_field_style(QtWidgets.QTimeEdit())
-               
+        # start_time_label.setMinimumSize()
         # Work end time
         end_time_label = set_default_label_style(QtWidgets.QLabel("Ende:"))
         end_time_field = set_default_field_style(QtWidgets.QTimeEdit())
@@ -233,8 +233,63 @@ class EditEntry(QtWidgets.QWidget):
             print("Debug: Close advanced entry settings.")
             self.fold_button.setArrowType(QtCore.Qt.RightArrow)
             for widget in changing_widgets: widget.hide()
-        
 
+class MonthOverview(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        months = ["Januar 2020","Februar 2020","März 2020","April 2020",
+                    "Mai 2020","Juni 2020","Juli 2020","August 2020",
+                    "September 2020","Oktober 2020","November 2020",
+                    "Dezember 2020"]
+
+        widget_content = QtWidgets.QVBoxLayout()
+
+        size_policy = QtWidgets.QSizePolicy()
+        # size_policy.setHorizontalPolicy(QtWidgets.QSizePolicy.Minimum)
+
+        entry_dict = {}
+
+        for month in months:
+            dict_name = month.replace(' ','_').lower()
+            entry_dict[dict_name] = {}
+            entry_dict[dict_name]['display'] = month[:3] + '\n' + month[-2:]
+            entry_dict[dict_name]['object'] = QtWidgets.QPushButton(entry_dict[dict_name]['display'])
+
+            entry_dict[dict_name]['object'].setFont(QtGui.QFont('SansSerif', pointSize=22))
+            entry_dict[dict_name]['object'].setStyleSheet("QPushButton {background: rgb(212, 212, 212)}")
+            entry_dict[dict_name]['object'].setMinimumWidth(130)
+            entry_dict[dict_name]['object'].setMaximumWidth(150)
+            entry_dict[dict_name]['object'].setMinimumHeight(200)
+            entry_dict[dict_name]['object'].setMaximumHeight(200)
+
+            widget_content.addWidget(entry_dict[dict_name]['object'])
+
+        widget_content.setAlignment(QtCore.Qt.AlignHCenter)
+        widget_content.setSpacing(20)
+
+        # Scroll Area
+        scroll_area_content = QtWidgets.QWidget()
+        scroll_area_content.setLayout(widget_content)
+        scroll_area = QtWidgets.QScrollArea()
+
+        scroll_area.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        scroll_area.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setWidget(scroll_area_content)
+        scroll_area.setStyleSheet("QScrollArea { background-color: white; border: none; }")
+
+
+        widget_line = QtWidgets.QFrame()
+        widget_line.setFrameShape(QtWidgets.QFrame.VLine)
+        widget_line.setFrameShadow(QtWidgets.QFrame.Sunken)
+
+        widget_layout = QtWidgets.QHBoxLayout()
+
+        widget_layout.addWidget(scroll_area)
+        widget_layout.addWidget(widget_line)
+
+        self.setLayout(widget_layout)
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -244,7 +299,12 @@ class MainWindow(QtWidgets.QMainWindow):
         edit_entry_widget.setMinimumWidth(400)
         edit_entry_widget.setMaximumWidth(450)
 
+        month_overview_widget = MonthOverview()
+        month_overview_widget.setMinimumWidth(150)
+        month_overview_widget.setMaximumWidth(250)
+
         layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(month_overview_widget)
         layout.addWidget(QtWidgets.QPushButton())#, stretch=8)
         layout.addWidget(edit_entry_widget)#, stretch=3)
 
