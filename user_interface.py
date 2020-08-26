3d71dc8a86346abdb034571cac553927b958c9e0
Jordan Alwon <jordan.alwon@student.jade-hs.de>
2020-08-26 09:30:44 +0200

    Add Content Widget

--- user_interface.py
+++ user_interface.py
@@ -291,6 +291,204 @@ class MonthOverview(QtWidgets.QWidget):
 
         self.setLayout(widget_layout)
 
+class EntryOverview(QtWidgets.QGraphicsView):
+    def __init__(self):
+        super().__init__()
+        width = 1150
+        height = 700
+        self.setMinimumSize(width,height)
+        self.setMaximumSize(width, height)
+        self.setSizePolicy(QtWidgets.QSizePolicy.Minimum,QtWidgets.QSizePolicy.Minimum)
+
+        scene = QtWidgets.QGraphicsScene(0,0,1130,680)
+        self.setFrameStyle(QtWidgets.QFrame.NoFrame)
+
+        scene.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(240,240,240), QtCore.Qt.BrushStyle.SolidPattern))
+        # self.setAlignment(QtCore.Qt.AlignHCenter)
+        
+        strong_pen = QtGui.QPen("black")
+        strong_pen.setWidthF(4)
+
+        default_pen = QtGui.QPen("black")
+        default_pen.setWidthF(2)
+
+        scene.addLine(10,200,1120,200, strong_pen)
+        scene.addLine(10,235 , 1120, 235, strong_pen)
+        scene.addLine(10,200,10,235, strong_pen)
+        scene.addLine(1120,200,1120,235, strong_pen)
+        scene.addLine(828,200,828,235, strong_pen)
+        scene.addLine(535,200,535,235, strong_pen)
+
+        scene.addLine(9,235,9,415, default_pen)
+        scene.addLine(535,230,535,415, default_pen)
+        scene.addLine(828,230,828,415, default_pen)
+        scene.addLine(1121,230,1121,415, default_pen)
+
+        scene.addLine(10,280,1120,280, default_pen)
+        scene.addLine(10,325,1120,325, default_pen)
+        scene.addLine(10,370,1120,370, default_pen)
+        scene.addLine(10,415,1120,415, default_pen)
+
+
+        scene.addLine(828,65,828,165, default_pen)
+        scene.addLine(555,115,1101,115, default_pen)
+        
+    
+
+        date_text = scene.addText("Datum")
+        date_text.setPos(230,200)
+        date_text.setFont(QtGui.QFont('SansSerif',pointSize=19))
+        # print(date_text.boundingRect().size())
+
+        work_hours = scene.addText("Arbeitsstunden")
+        work_hours.setPos(590,200)
+        work_hours.setFont(QtGui.QFont('SansSerif', pointSize=19))
+
+        earnings_text = scene.addText("Verdienst")
+        earnings_text.setPos(915,200)
+        earnings_text.setFont(QtGui.QFont('SansSerif', pointSize=19))
+
+        entry_date_text = scene.addText("Di. 04.08.2020")
+        entry_date_text.setPos(170,238)
+        entry_date_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        entry_hours_text = scene.addText("6 Stunden")
+        entry_hours_text.setPos(610,238)
+        entry_hours_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        entry_earnings_text = scene.addText("60 €")
+        entry_earnings_text.setPos(950,238)
+        entry_earnings_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        entry2_date_text = scene.addText("Do. 06.08.2020")
+        entry2_date_text.setPos(162,283)
+        entry2_date_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        entry2_hours_text = scene.addText("10 Stunden")
+        entry2_hours_text.setPos(595,283)
+        entry2_hours_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        entry2_earnings_text = scene.addText("100 €")
+        entry2_earnings_text.setPos(935,283)
+        entry2_earnings_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+
+        month_title_text = scene.addText("August 2020")
+        month_title_text.setPos(70,50)
+        month_title_text.setFont(QtGui.QFont('SansSerif', pointSize=50))
+
+        sum_hours_text = scene.addText("∑ Arbeitsstunden")
+        sum_hours_text.setPos(570,70)
+        sum_hours_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        sum_earnings_text = scene.addText("∑ Verdienst")
+        sum_earnings_text.setPos(880,70)
+        sum_earnings_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        sum_hours_value_text = scene.addText("16 Stunden")
+        sum_hours_value_text.setPos(595,120)
+        sum_hours_value_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        sum_earnings_value_text = scene.addText("160 €")
+        sum_earnings_value_text.setPos(932,120)
+        sum_earnings_value_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+
+        scene.addLine(210,500,828,500, default_pen)
+        scene.addLine(210,555,828,555, default_pen)
+        scene.addLine(210,610,828,610, default_pen)
+        scene.addLine(210,665,828,665, default_pen)
+
+        scene.addLine(210,500,210,665, default_pen)
+        scene.addLine(535,500,535,665, default_pen)
+        scene.addLine(828,500,828,665, default_pen)
+
+
+        overtime_text = scene.addText("Überstunden")
+        overtime_text.setPos(175,660)
+        overtime_text.setRotation(-90)
+        overtime_text.setFont(QtGui.QFont('SansSerif', pointSize=19))
+
+        pre_overtime_text = scene.addText("Vormonat (Juli)")
+        pre_overtime_text.setPos(250,505)
+        pre_overtime_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        pre_overtime_text = scene.addText("Neu")
+        pre_overtime_text.setPos(320,560)
+        pre_overtime_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        pre_overtime_text = scene.addText("Gesamt")
+        pre_overtime_text.setPos(290,615)
+        pre_overtime_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        pre_overtime_value_text = scene.addText("50 Stunden")
+        pre_overtime_value_text.setPos(600,505)
+        pre_overtime_value_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        pre_overtime_text = scene.addText("6 Stunden")
+        pre_overtime_text.setPos(617,560)
+        pre_overtime_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        pre_overtime_text = scene.addText("56 Stunden")
+        pre_overtime_text.setPos(600,615)
+        pre_overtime_text.setFont(QtGui.QFont('SansSerif', pointSize=23))
+
+        self.setScene(scene)
+
+
+class ContentArea(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        widget_layout = QtWidgets.QVBoxLayout()
+
+        entry_overview_widget = EntryOverview()
+        
+        widget_layout.setAlignment(QtCore.Qt.AlignHCenter)
+
+        widget_layout.addWidget(entry_overview_widget)
+        widget_layout.addSpacing(800)
+
+        button_layout = QtWidgets.QHBoxLayout()
+        button_layout.addSpacing(200)
+        button_layout.setSpacing(30)
+
+        add_button = QtWidgets.QPushButton("Add")
+        add_button.setFont(QtGui.QFont('SansSerif', pointSize=16))
+        add_button.setFixedHeight(40)
+        add_button.setFixedWidth(100)
+        button_layout.addWidget(add_button)
+
+        overtime_button = QtWidgets.QPushButton("OT")
+        overtime_button.setFont(QtGui.QFont('SansSerif', pointSize=16))
+        overtime_button.setFixedHeight(40)
+        overtime_button.setFixedWidth(100)
+        button_layout.addWidget(overtime_button)
+
+        close_month_button = QtWidgets.QPushButton("Abschließen")
+        close_month_button.setFont(QtGui.QFont('SansSerif', pointSize=16))
+        close_month_button.setFixedHeight(40)
+        close_month_button.setFixedWidth(100)
+        button_layout.addWidget(close_month_button)
+
+        export_button = QtWidgets.QPushButton("Export")
+        export_button.setFont(QtGui.QFont('SansSerif', pointSize=16))
+        export_button.setFixedHeight(40)
+        export_button.setFixedWidth(100)
+        button_layout.addWidget(export_button)
+
+        options_button = QtWidgets.QPushButton("Optiones")
+        options_button.setFont(QtGui.QFont('SansSerif', pointSize=16))
+        options_button.setFixedHeight(40)
+        options_button.setFixedWidth(100)
+        button_layout.addWidget(options_button)
+
+        button_layout.addSpacing(200)
+
+        widget_layout.addLayout(button_layout)
+
+        self.setLayout(widget_layout)
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -303,9 +501,11 @@ class MainWindow(QtWidgets.QMainWindow):
         month_overview_widget.setMinimumWidth(150)
         month_overview_widget.setMaximumWidth(250)
 
+        content_area = ContentArea()
+
         layout = QtWidgets.QHBoxLayout()
         layout.addWidget(month_overview_widget)
-        layout.addWidget(QtWidgets.QPushButton())#, stretch=8)
+        layout.addWidget(content_area)
         layout.addWidget(edit_entry_widget)#, stretch=3)
 
 
